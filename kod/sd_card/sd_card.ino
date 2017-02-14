6d2b50dcbd13e02c16ab975921acfb26ae7491f7
Johan Kämpe <kampe.johan@gmail.com>
2017-02-14 11:36:13 +0100

    Updated code to write to textfile per cycle

--- kod/sd_card/sd_card.ino
+++ kod/sd_card/sd_card.ino
@@ -21,7 +21,9 @@
 #include <SPI.h>
 #include <SD.h>
 
-File myFile;
+File logfile;
+#define CS_PIN 10
+int rad = 1;
 
 void setup() {
   // Open serial communications and wait for port to open:
@@ -33,45 +35,37 @@ void setup() {
 
   Serial.print("Initializing SD card...");
 
-  if (!SD.begin(4)) {
+  if (!SD.begin(CS_PIN)) {
     Serial.println("initialization failed!");
     return;
   }
   Serial.println("initialization done.");
-
-  // open the file. note that only one file can be open at a time,
-  // so you have to close this one before opening another.
-  myFile = SD.open("test.txt", FILE_WRITE);
-
-  // if the file opened okay, write to it:
-  if (myFile) {
-    Serial.print("Writing to test.txt...");
-    myFile.println("testing 1, 2, 3.");
-    // close the file:
-    myFile.close();
-    Serial.println("done.");
-  } else {
-    // if the file didn't open, print an error:
-    Serial.println("error opening test.txt");
-  }
-
-  // re-open the file for reading:
-  myFile = SD.open("test.txt");
-  if (myFile) {
-    Serial.println("test.txt:");
-
-    // read from the file until there's nothing else in it:
-    while (myFile.available()) {
-      Serial.write(myFile.read());
-    }
-    // close the file:
-    myFile.close();
-  } else {
-    // if the file didn't open, print an error:
-    Serial.println("error opening test.txt");
-  }
 }
 
 void loop() {
-  // nothing happens after setup
+  SD.remove("log.txt");
+  logfile = SD.open("log.txt", FILE_WRITE);
+  logfile.println("----------------------");
+  for(int i = 0; i< 20; i++){
+    logfile.print("Rad ");
+    logfile.print(rad++);
+    logfile.println(" testText");
+  }
+  logfile.println("----------------------");
+  logfile.close();
+  logfile = SD.open("log.txt");
+  if(logfile){
+    Serial.println("log.txt:");
+    // read from the file until there's nothing else in it:
+    while (logfile.available()) {
+      Serial.write(logfile.read());
+    }
+    //close the file:
+    logfile.close();
+  }
+  else{
+    // if the file didn't open, print an error:
+    Serial.println("error opening log.txt");
+  }
+  delay(1000);
 }
