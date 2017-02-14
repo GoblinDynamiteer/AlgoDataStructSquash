341efb53a48353c5c5711265a0a53eee805de48b
Unknown <dylansaleh98@gmail.com>
2017-02-14 14:52:21 +0100

    squash_gyro version 2 with calibration

--- kod/squash_gyro/squash_gyro2/squash_gyro2.ino
+++ kod/squash_gyro/squash_gyro2/squash_gyro2.ino
@@ -8,49 +8,71 @@ int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
 uint16_t ticker = 0;
 bool fore = 0;
 bool back = 0;
+uint16_t diff = 0;
+int countdown = 10;
 
 void setup(){
   Wire.begin();
   Wire.beginTransmission(MPU_addr);
+  Wire.write(0x6B);  // PWR_MGMT_1 register
   Wire.write(0);     // set to zero (wakes up the MPU-6050)
   Wire.endTransmission(true);
   Serial.begin(9600);
+
+  Serial.print("Calibrating completed in ");
+  for(int i = 0; i < 10; i++){
+    Wire.beginTransmission(MPU_addr);
+    Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+    Wire.endTransmission(false);
+    Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
+    AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+
+    diff = diff + AcZ;
+    delay(1000);
+    Serial.println(countdown--);
+    Serial.println(AcZ);
+  }
+  diff = diff /10;
 }
 void loop(){
   Wire.beginTransmission(MPU_addr);
   Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
   Wire.endTransmission(false);
   Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
-  AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
-  Wire.write(0x6B);  // PWRead();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
-  AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+//  AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
+//  AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
   AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
-  Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
-  GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
-  GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
-  GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
-
-  AcZ = ((AcZ + 16200)/10);
+//  Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+//  GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+//  GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+//  GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
 
 
-  if(AcZ < -3200 && back == 0){
+  
+
+  AcZ = ((AcZ - diff));
+//  Serial.println(AcZ);
+//  Serial.print("diff = ");
+//  Serial.println(diff);
+
+
+  if(AcZ < -3300 && back == 0){
     Serial.println("Forehand");
     Serial.println(AcZ);
     fore = 1;
-    if(AcZ > 3200){
+    if(AcZ > 3300){
       fore = 0;
     }
   }
 
-  if(AcZ > 3200 && fore == 0){
+  if(AcZ > 3300 && fore == 0){
     Serial.println("Backhand");
     Serial.println(AcZ);
     back = 1;
-    if(AcZ < -3200){
+    if(AcZ < -3300){
       back = 0;
     }
   }
-
   //Serial.println(ticker);
   
 //  Serial.print("AcX = ");
