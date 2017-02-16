015cbc9cffa2a619833f9c05d7297fd2053a7ed1
Johan Kämpe <kampe.johan@gmail.com>
2017-02-16 13:28:47 +0100

    Added array to hold data

--- kod/squash_gyro/squash_gyro2/squash_gyro2.ino
+++ kod/squash_gyro/squash_gyro2/squash_gyro2.ino
@@ -3,6 +3,23 @@ const int MPU_addr = 0x68;  // I2C address of the MPU-6050
 int16_t AcZ;
 uint16_t ticker = 0;
 uint16_t diff = 0;
+int time;
+
+/*  Struct för hållande av slag */
+typedef struct{
+  int16_t styrka;
+  int tid;
+  int langd;
+  bool typ;
+}slagS;
+
+enum{FOREHAND, BACKHAND};
+#define MAXSLAG 10
+
+/*  Struct array */
+slagS slag[MAXSLAG];
+byte antal = 0;
+int ix = 0;
 
 void readMPUData(){
     Wire.beginTransmission(MPU_addr);
@@ -15,10 +32,12 @@ void readMPUData(){
 void setup(){
   Wire.begin();
   Wire.beginTransmission(MPU_addr);
-  Wire.write(0x6B);  // PWR_MGMT_1 register
-  Wire.write(0);     // set to zero (wakes up the MPU-6050)
+  Wire.write(0x6B);
+  Wire.write(0);
   Wire.endTransmission(true);
   Serial.begin(9600);
+  int countdown = 10;
+  /*  Hitta diff-värde för kalibration   */
   Serial.print("Calibrating completed in ");
   for(int i = 0; i < 10; i++){
     readMPUData();
@@ -33,9 +52,12 @@ void loop(){
   readMPUData();
   AcZ = ((AcZ - diff));
   if(AcZ < -3300){
-    int time = millis();
+    time = millis();
     Serial.println("Forehand");
     Serial.println(AcZ);
+    slag[ix].styrka = fabs(AcZ);
+    slag[ix].tid = (int)time;
+    slag[ix].typ = FOREHAND;
     while(AcZ < 3300){
         readMPUData();
         AcZ = ((AcZ - diff));
@@ -43,13 +65,18 @@ void loop(){
     time = millis() - time;
     Serial.println("tid för slag:");
     Serial.print(time);
+    slag[ix].langd = (int)time;
+    ix++;
     delay(2000);
   }
 
   if(AcZ > 3300){
-    int time = millis();
+    time = millis();
     Serial.println("Backhand");
     Serial.println(AcZ);
+    slag[ix].styrka = fabs(AcZ);
+    slag[ix].tid = (int)time;
+    slag[ix].typ = BACKHAND;
     while(AcZ > -3300){
         readMPUData();
         AcZ = ((AcZ - diff));
@@ -57,7 +84,12 @@ void loop(){
     time = millis() - time;
     Serial.println("tid för slag:");
     Serial.print(time);
+    slag[ix].langd = (int)time;
+    ix++;
     delay(2000);
+  }
+  if(ix == 100){
+      Serial.println("DONE!");
   }
   delay(10);
 }
