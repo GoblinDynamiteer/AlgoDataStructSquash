5f9d5e44122190eb0d0da853a0c6240543b40781
Johan Kämpe <kampe.johan@gmail.com>
2017-02-16 13:04:00 +0100

    Cut down code size

--- kod/squash_gyro/squash_gyro2/squash_gyro2.ino
+++ kod/squash_gyro/squash_gyro2/squash_gyro2.ino
@@ -1,15 +1,16 @@
-// MPU-6050 Short Example Sketch
-// By Arduino User JohnChi
-// August 17, 2014
-// Public Domain
-#include<Wire.h>
-const int MPU_addr=0x68;  // I2C address of the MPU-6050
-int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+#include <Wire.h>
+const int MPU_addr = 0x68;  // I2C address of the MPU-6050
+int16_t AcZ;
 uint16_t ticker = 0;
-bool fore = 0;
-bool back = 0;
 uint16_t diff = 0;
-int countdown = 10;
+
+void readMPUData(){
+    Wire.beginTransmission(MPU_addr);
+    Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+    Wire.endTransmission(false);
+    Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
+    AcZ = Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+}
 
 void setup(){
   Wire.begin();
@@ -18,106 +19,45 @@ void setup(){
   Wire.write(0);     // set to zero (wakes up the MPU-6050)
   Wire.endTransmission(true);
   Serial.begin(9600);
-
   Serial.print("Calibrating completed in ");
   for(int i = 0; i < 10; i++){
-    Wire.beginTransmission(MPU_addr);
-    Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
-    Wire.endTransmission(false);
-    Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
-    AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
-
+    readMPUData();
     diff = diff + AcZ;
     delay(1000);
     Serial.println(countdown--);
     Serial.println(AcZ);
   }
-  diff = diff /10;
+  diff = diff / 10;
 }
 void loop(){
-  Wire.beginTransmission(MPU_addr);
-  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
-  Wire.endTransmission(false);
-  Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
-//  AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
-//  AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
-  AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
-//  Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
-//  GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
-//  GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
-//  GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
-
-
-  
-
+  readMPUData();
   AcZ = ((AcZ - diff));
-//  Serial.println(AcZ);
-//  Serial.print("diff = ");
-//  Serial.println(diff);
-
-
   if(AcZ < -3300){
+    int time = millis();
     Serial.println("Forehand");
     Serial.println(AcZ);
-    fore = 1;
     while(AcZ < 3300){
-        Wire.beginTransmission(MPU_addr);
-        Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
-        Wire.endTransmission(false);
-        Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
-        AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+        readMPUData();
         AcZ = ((AcZ - diff));
     }
+    time = millis() - time;
     Serial.println("tid för slag:");
-    delay(3000);
+    Serial.print(time);
+    delay(2000);
   }
-  
-//  if(AcZ > 3300 && fore == 1){
-//    fore = 0;
-////    Serial.println("tid för slag:");
-////    Serial.print();
-//    delay(10000);
-//  }
 
   if(AcZ > 3300){
     int time = millis();
     Serial.println("Backhand");
     Serial.println(AcZ);
-    back = 1;
     while(AcZ > -3300){
-        Wire.beginTransmission(MPU_addr);
-        Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
-        Wire.endTransmission(false);
-        Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
-        AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+        readMPUData();
         AcZ = ((AcZ - diff));
     }
-    time = millis() - time;      
-
+    time = millis() - time;
     Serial.println("tid för slag:");
     Serial.print(time);
-    
-    delay(3000);
+    delay(2000);
   }
-//  if(AcZ < -3300 && back == 1){
-//    back = 0;
-////    Serial.println("tid för slag:");
-////    Serial.print();
-//    delay(10000);
-//  }
-  
-  //Serial.println(ticker);
-  
-//  Serial.print("AcX = ");
-  //Serial.println(AcX);
-//  Serial.print(" | AcY = "); 
-//Serial.println(AcY);
-//  Serial.print(" | AcZ = "); 
-//Serial.println(AcZ);
-  //Serial.print(" | Tmp = "); Serial.print(Tmp/340.00+36.53);  //equation for temperature in degrees C from datasheet
-/*   Serial.print(" | GyX = "); Serial.print(GyX);
-  Serial.print(" | GyY = "); Serial.print(GyY);
-  Serial.print(" | GyZ = "); Serial.println(GyZ); 
-  Serial.println(GyX); */
   delay(10);
 }
